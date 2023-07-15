e52128f09ac98c538b357ea062bb7cf0773ec310
ilvikaMitt <139557754+ilvikaMitt@users.noreply.github.com>
2023-07-15 10:46:18 +0530

    Add files via upload

---
+++ canvas.py
@@ -0,0 +1,37 @@
+from tkinter import*
+from PIL import ImageTk,Image
+root=Tk()
+
+root.geometry("600x600")
+root.title("canvas")
+
+color=Label(root,text="Enter color")
+color.place(relx=0.6,rely=0.9,anchor=CENTER)
+
+color_input=Entry(root)
+color_input.insert(0,"green")
+color_input.place(relx=0.8,rely=0.9,anchor=CENTER)
+
+canvas=Canvas(root,width=590,height=510,background="white",highlightbackground="lightgrey")
+canvas.pack()
+
+start_img=ImageTk.PhotoImage(Image.open("start_point1.png"))
+myImage=canvas.create_image(50,50,image=start_img)
+
+direction=""
+old_x=50
+old_y=50
+new_x=50
+new_y=50
+
+def draw(direction,old_x,old_y,new_x,new_y):
+    fill_color=color_input.get()
+ 
+#root.bind("<Right>",right_dir)
+#root.bind("<Left>",left_dir)
+#root.bind("<Down>",down_dir)    
+#root.bind("<Up>",up_dir)
+
+    
+
+root.mainloop()
